57c0ff28318b18b940451d8d32842a83dd0c96ec
Noe Carl <noe.carldev@gmail.com>
2023-04-02 14:30:29 +0200

    default theming and fonts done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: [
+				'MotivaSans',
+				'system-ui',
+				'-apple-system',
+				'BlinkMacSystemFont',
+				'"Segoe UI"',
+				'Roboto',
+				'Helvetica',
+				'Arial',
+				'sans-serif',
+			],
+		},
+		extend: {
+			colors: {
+				steam: {
+					dark: '#171a21',
+					light: '#c7d5e0',
+					lightBlue: '#66c0f4',
+					darkBlue: '#1b2838',
+					medBlue: '#2a475e',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
