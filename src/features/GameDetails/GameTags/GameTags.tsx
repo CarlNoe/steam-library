f2672845445e5b8dc4397b94a6bb3277bb1b14d5
Noe Carl <noe.carldev@gmail.com>
2023-04-05 23:19:40 +0200

    Fixed tags in game details

--- src/features/GameDetails/GameTags/GameTags.tsx
+++ src/features/GameDetails/GameTags/GameTags.tsx
@@ -9,11 +9,11 @@ function GameTags(props: GameTagsProps) {
 	return (
 		<div className="mb-4">
 			<h2 className="mb-1 text-base opacity-60">TAGS</h2>
-			<div>
+			<div className="flex flex-wrap">
 				{splitBySemicolon(genres).map((genre) => (
 					<span
 						key={genre}
-						className="mr-2 rounded-md bg-steam-medBlue bg-opacity-50 px-3 py-[6px] text-xs text-steam-lightBlue"
+						className="mb-2 mr-2 w-fit whitespace-nowrap rounded-md bg-steam-medBlue bg-opacity-50 px-3 py-[6px] text-xs text-steam-lightBlue"
 					>
 						{genre}
 					</span>
