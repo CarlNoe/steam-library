c8f5ad29683b61017af201e56893b43aab42d62b
suncher <krebsjerem@gmail.com>
2023-04-05 12:35:48 +0200

    form

--- src/features/SignIn/signInForm.tsx
+++ src/features/SignIn/signInForm.tsx
@@ -2,62 +2,75 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function SignInForm() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-    const handleSubmit = (e: { preventDefault: () => void; }) => {
-        e.preventDefault();
-        // Handle sign-in submission here
-    };
+	const handleSubmit = (e: { preventDefault: () => void }) => {
+		e.preventDefault();
+		// Handle sign-in submission here
+	};
 
-    return (
-        <div className="min-h-screen  flex items-center justify-center">
-            <div className="bg-steam-grey rounded-lg shadow-lg px-12 py-8">
-                <h1 className="text-3xl font-medium text-white mb-8">Sign In</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-6">
-                        <label htmlFor="email" className="block text-gray-300 font-medium mb-2">Email address</label>
-                        <input
-                            aria-label="Email address"
-                            id="email"
-                            name="email"
-                            type="email"
-                            required
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="appearance-none border border-gray-600 rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:border-indigo-500"
-                            placeholder="you@example.com"
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="password" className="block text-gray-300 font-medium mb-2">Password</label>
-                        <input
-                            aria-label="Password"
-                            id="password"
-                            name="password"
-                            type="password"
-                            required
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="appearance-none border border-gray-600 rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:border-indigo-500"
-                            placeholder="********"
-                        />
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <button
-                            type="submit"
-                            className="bg-indigo-500 hover:bg-indigo-400 text-white font-medium py-2 px-6 rounded focus:outline-none focus:shadow-outline-indigo"
-                        >
-                            Sign In
-                        </button>
-                        <Link to="/SignUp" className="ml-4 hover:text-indigo-400 font-medium">
-                            Don't have an account? Sign up
-                        </Link>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+	return (
+		<div className="flex  min-h-screen items-center justify-center">
+			<div className="rounded-lg bg-steam-grey px-12 py-8 shadow-lg">
+				<h1 className="mb-8 text-3xl font-medium text-white">Sign In</h1>
+				<form onSubmit={handleSubmit}>
+					<div className="mb-6">
+						<label
+							htmlFor="email"
+							className="mb-2 block font-medium text-gray-300"
+						>
+							Email address
+						</label>
+						<input
+							aria-label="Email address"
+							id="email"
+							name="email"
+							type="email"
+							required
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							className="w-full appearance-none rounded border border-gray-600 px-3 py-2 leading-tight text-white focus:border-indigo-500 focus:outline-none"
+							placeholder="you@example.com"
+						/>
+					</div>
+					<div className="mb-6">
+						<label
+							htmlFor="password"
+							className="mb-2 block font-medium text-gray-300"
+						>
+							Password
+						</label>
+						<input
+							aria-label="Password"
+							id="password"
+							name="password"
+							type="password"
+							required
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							className="w-full appearance-none rounded border border-gray-600 px-3 py-2 leading-tight text-white focus:border-indigo-500 focus:outline-none"
+							placeholder="********"
+						/>
+					</div>
+					<div className="flex items-center justify-between">
+						<button
+							type="submit"
+							className="focus:shadow-outline-indigo rounded bg-indigo-500 px-6 py-2 font-medium text-white hover:bg-indigo-400 focus:outline-none"
+						>
+							Sign In
+						</button>
+						<Link
+							to="/SignUp"
+							className="ml-4 font-medium hover:text-indigo-400"
+						>
+							Don&apos;t have an account? Sign up
+						</Link>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default SignInForm;
