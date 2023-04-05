360738cc43568272f351d02001bad465dcf3c702
Noe Carl <noe.carldev@gmail.com>
2023-04-05 22:11:31 +0200

    Added data for empty values in game tile

--- src/common/GameTile/GameTile.tsx
+++ src/common/GameTile/GameTile.tsx
@@ -37,20 +37,35 @@ const GameTile = React.memo((props: GameTileProps) => {
 		<Link to={link}>
 			<div className="mb-2 flex h-20 bg-steam-dark bg-opacity-40 p-2">
 				<div className="flex flex-shrink-0 flex-col justify-between">
-					<img src={headerImage} alt={name} className="h-12" />
+					<img
+						src={
+							headerImage ||
+							'https://fakeimg.pl/350x200/?text=NoImage&font=lobster'
+						}
+						alt={name}
+						className="h-12"
+					/>
 					<div className="flex items-center justify-between font-motiva text-[11px]">
-						<span className="opacity-[50%]">{releaseDate}</span>
-						<span className="text-steam-lightBlue">{averageRating}%</span>
+						<span className="opacity-[50%]">{releaseDate || 'TBA'}</span>
+						<span className="text-steam-lightBlue">
+							{Number(positiveRatings) > 0 && Number(negativeRatings) > 0
+								? `${averageRating}%`
+								: 'No ratings'}
+						</span>
 					</div>
 				</div>
 				<div className="ml-2 flex w-full flex-col justify-between">
 					<h3 className="font-motiva">{name}</h3>
 					<div className="flex opacity-50">
-						{platforms.includes('windows') && (
+						{typeof platforms === 'string' && platforms.includes('windows') && (
 							<img src={windows} alt="Windows" />
 						)}
-						{platforms.includes('mac') && <img src={mac} alt="Mac" />}
-						{platforms.includes('linux') && <img src={linux} alt="Linux" />}
+						{typeof platforms === 'string' && platforms.includes('mac') && (
+							<img src={mac} alt="Mac" />
+						)}
+						{typeof platforms === 'string' && platforms.includes('linux') && (
+							<img src={linux} alt="Linux" />
+						)}
 					</div>
 					<span className="self-end text-xs font-bold">
 						{Number(price) > 0 ? `${price}€` : 'Free-to-play'}
